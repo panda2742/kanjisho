44fea5aa6a0bce29039aeae670c02643f5d1b0e2
panda2742 <elouannhosta27@gmail.com>
2025-08-12 18:42:12 +0200

    Basic kanji structure

--- include/Kanji.hpp
+++ include/Kanji.hpp
@@ -1,23 +1,27 @@
 #ifndef KANJI_HPP
-# define KANJI_HPP
+#define KANJI_HPP
 
-# include <string>
+#include <string>
+#include <map>
+#include <vector>
+
+using namespace	std;
 
 /**
  * Represent a Kanji with all its attribute.
  */
 typedef struct Kanji
 {
-	std::string	literal;
-	struct s_codepoint
-	{
-		std::unord<>;
-	};
-	radical;
-	misc;
-	dic_number;
-	query_code;
-	reading_meaning;
+	u8string					literal;
+	vector<u8string>			kun_readings;
+	vector<u8string>			on_readings;
+	vector<u8string>			name_readings;
+	map<string, vector<string>>	meanings;
+	unsigned char				stroke_count;
+	u8string					unicode;
+	unsigned char				grade;
+	unsigned char				former_jlpt;
+	unsigned short int			freq;
 }	Kanji_t;
 
 #endif
